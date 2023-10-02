8c360ca3ef3ebb03c8b4efb696c3366bcb42cefe
sckimynwa <tsb03672@naver.com>
2023-10-02 20:12:39 +0900

    fix: remove studio lesson type

--- src/pages/product/ProductStudio.option.jsx
+++ src/pages/product/ProductStudio.option.jsx
@@ -1,17 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import { setCreatedAt } from '../../constants';
+import { setCreatedAt } from "../../constants";
 
 export const LIST_HEADER = [
-  { id: 'lessonType', label: '분류' },
   {
-    id: 'title',
-    label: '수업명',
+    id: "title",
+    label: "수업명",
     customBodyRender: (value, data) => {
       return (
-        <Link to={`/pages/product/studio/${data?.studiosId || ''}/${data?.id || ''}`}>
-          <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-            <img src={data?.images[0]?.url || ''} style={{ width: 49, height: 49 }} />
+        <Link
+          to={`/pages/product/studio/${data?.studiosId || ""}/${
+            data?.id || ""
+          }`}
+        >
+          <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+            <img
+              src={data?.images[0]?.url || ""}
+              style={{ width: 49, height: 49 }}
+            />
             {value}
           </div>
         </Link>
@@ -19,59 +25,59 @@ export const LIST_HEADER = [
     },
   },
   {
-    id: 'isShow',
-    label: '게시여부',
+    id: "isShow",
+    label: "게시여부",
     customBodyRender: (value) => {
-      return value ? <p className="point-text">게시중</p> : '숨김';
+      return value ? <p className="point-text">게시중</p> : "숨김";
     },
   },
   {
-    id: 'createdAt',
-    label: '등록일시',
+    id: "createdAt",
+    label: "등록일시",
     customBodyRender: (value) => {
-      return setCreatedAt(value, '-');
+      return setCreatedAt(value, "-");
     },
   },
   {
-    id: 'updatedAt',
-    label: '수정일시',
+    id: "updatedAt",
+    label: "수정일시",
     customBodyRender: (value) => {
-      return setCreatedAt(value, '-');
+      return setCreatedAt(value, "-");
     },
   },
 ];
 
 export const EXEL_HEADER = [
-  { id: 'lessonType', label: '분류' },
+  { id: "lessonType", label: "분류" },
   {
-    id: 'title',
-    label: '수업명',
+    id: "title",
+    label: "수업명",
   },
   {
-    id: 'isShow',
-    label: '게시여부',
+    id: "isShow",
+    label: "게시여부",
     customBodyRender: (value) => {
-      return value ? '게시중' : '숨김';
+      return value ? "게시중" : "숨김";
     },
   },
   {
-    id: 'createdAt',
-    label: '등록일시',
+    id: "createdAt",
+    label: "등록일시",
     customBodyRender: (value) => {
-      return setCreatedAt(value, '-');
+      return setCreatedAt(value, "-");
     },
   },
   {
-    id: 'updatedAt',
-    label: '수정일시',
+    id: "updatedAt",
+    label: "수정일시",
     customBodyRender: (value) => {
-      return setCreatedAt(value, '-');
+      return setCreatedAt(value, "-");
     },
   },
 ];
 
 export const FILTER = [
-  { id: '', label: '전체' },
-  { id: 'Special', label: 'Special' },
-  { id: 'Regular', label: 'Regular' },
+  { id: "", label: "전체" },
+  { id: "Special", label: "Special" },
+  { id: "Regular", label: "Regular" },
 ];
